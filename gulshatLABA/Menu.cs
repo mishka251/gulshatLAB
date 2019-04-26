e7a8daeb347ba7c38bdf1a29d73b9172811f8b97
Mikhail Belov <mbelov@os-company.ru>
2019-04-27 01:20:28 +0500

    init

---
+++ gulshatLABA/Menu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace gulshatLABA
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+
+            InitializeComponent();
+            myDB.Init();
+        }
+
+
+       
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Zakaz z = new Zakaz();
+            z.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Tovar t = new Tovar();
+            t.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Shop s = new Shop();
+            s.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Employe e1 = new Employe();
+            e1.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Client c = new Client();
+            c.Show();
+        }
+    }
+}
---
+++ gulshatLABA/myDB.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Data;
+
+namespace gulshatLABA
+{
+    static class myDB
+    {
+        static SqlConnection sqlConnection;
+        static myDB()
+        {
+            var curDir = Directory.GetCurrentDirectory();
+            var projDir = Directory.GetParent(curDir).Parent.FullName;
+
+
+            SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder
+            {
+                DataSource = @"(LocalDB)\MSSQLLocalDB",
+                AttachDBFilename = projDir + @"\newDB.mdf",
+                IntegratedSecurity = true
+            };
+
+            sqlConnection = new SqlConnection
+            {
+                ConnectionString = sb.ConnectionString
+            };
+
+            sqlConnection.Open();
+            if (sqlConnection.State != ConnectionState.Open)
+            {
+                MessageBox.Show("Ошибка подключения к БД. Приложение будет закрыто");
+                Application.Exit();
+            }
+
+        }
+
+        public static void Init()
+        {
+
+        }
+
+        public static DataTable SELECT(
+            List<string> fields,
+            List<string> tables,
+            List<string> conditions = null
+            )
+        {
+
+            string f = String.Join(", ", fields);
+            string t = String.Join(", ", tables);
+            string cond = conditions == null ? "" : "WHERE " + String.Join(" AND ", conditions);
+
+            string SQL = $"SELECT {f} FROM {t} {cond}";
+
+            DataTable res = new DataTable();
+
+
+            SqlCommand command = sqlConnection.CreateCommand();
+            command.CommandText = SQL;
+            var reader = command.ExecuteReader();
+            int cols = reader.FieldCount;
+
+            for (int i = 0; i < cols; i++)
+                res.Columns.Add(new DataColumn(fields[i]));
+
+            object[] row = new object[cols];
+            while (reader.Read())
+            {
+                for (int i = 0; i < cols; i++)
+                    row[i] = reader[i];
+
+                res.Rows.Add(row);
+            }
+            reader.Close();
+            return res;
+        }
+
+
+        public static DataTable SELECT_ALL(
+   string table,
+    List<string> conditions = null
+    )
+        {
+
+            string cond = conditions == null ? "" : "WHERE " + String.Join(" AND ", conditions);
+
+            string SQL = $"SELECT * FROM {table} {cond}";
+
+            DataTable res = new DataTable();
+
+
+            SqlCommand command = sqlConnection.CreateCommand();
+            command.CommandText = SQL;
+            var reader = command.ExecuteReader();
+            int cols = reader.FieldCount;
+
+            for (int i = 0; i < cols; i++)
+                res.Columns.Add(new DataColumn(reader.GetName(i)));
+
+            object[] row = new object[cols];
+            while (reader.Read())
+            {
+                for (int i = 0; i < cols; i++)
+                    row[i] = reader[i];
+
+                res.Rows.Add(row);
+            }
+            reader.Close();
+            return res;
+        }
+
+    }
+}
